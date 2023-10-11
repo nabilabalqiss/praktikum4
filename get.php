aa09e2453158743a934f8727301f5a9f7da4037b
nabilabalqiss <nabila11balqis@gmail.com>
2023-10-11 22:46:26 +0700

    first commit

---
+++ get.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="proses.php">
+        <label>Nilai 1</label>
+        <input type="text" name="nilai1">
+        <br />
+        <label>Nilai 2</label>
+        <input type="text" name="nilai2">
+
+        <input type="submit" value="PROSES"/>
+    </form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,7 @@
+<?php
+   echo $_GET ['nilai1']. " dan " .$_GET ['nilai2']. "<br />";
+   $nilai1=$_GET['nilai1'];
+   $nilai2=$_GET['nilai2'];
+
+   echo " Jumlahnya adalah ".$nilai1." ditambah dengan ".$nilai2." adalah ".$nilai1+$nilai2;
+?>
